9a2451ced8fa25462b8f3b25e7d40f1ef7853de2
TedtheDev <tylerjedmunds@gmail.com>
2016-10-01 20:10:34 -0500

    created hello world react app but issue with babel transpiling

---
+++ app/index.js
@@ -0,0 +1,15 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+const HelloWorld = React.createClass({
+  render() {
+    return (
+      <div>Hello World</div>
+    );
+  }
+});
+
+ReactDOM.render(
+  <HelloWorld />,
+  document.getElementById('app')
+);
